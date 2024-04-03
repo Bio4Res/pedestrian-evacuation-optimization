c637c41168b443f30312b616648feeaca956c26c
Carlos Cotta <ccottap@gmail.com>
2024-04-03 09:35:43 +0100

    Remove unused import

--- src/main/java/es/uma/lcc/caesium/pedestrian/evacuation/optimization/ea/RunEvacuationOptimization.java
+++ src/main/java/es/uma/lcc/caesium/pedestrian/evacuation/optimization/ea/RunEvacuationOptimization.java
@@ -1,6 +1,5 @@
 package es.uma.lcc.caesium.pedestrian.evacuation.optimization.ea;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
