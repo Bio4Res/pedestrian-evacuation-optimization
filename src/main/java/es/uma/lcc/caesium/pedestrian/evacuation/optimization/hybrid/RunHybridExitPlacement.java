df7d69b86b3e43e8ad6fc748a4af3ba3e943526a
Carlos Cotta <ccottap@gmail.com>
2024-04-03 09:35:15 +0100

    Remove unused imports

--- src/main/java/es/uma/lcc/caesium/pedestrian/evacuation/optimization/hybrid/RunHybridExitPlacement.java
+++ src/main/java/es/uma/lcc/caesium/pedestrian/evacuation/optimization/hybrid/RunHybridExitPlacement.java
@@ -1,6 +1,5 @@
 package es.uma.lcc.caesium.pedestrian.evacuation.optimization.hybrid;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
