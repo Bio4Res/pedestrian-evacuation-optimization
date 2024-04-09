b3cf61f471c8b1ea68cccf17a3a06800c676a4df
Carlos Cotta <ccottap@gmail.com>
2024-04-09 15:03:33 +0200

    Added memetic algorithm resolution

---
+++ src/main/java/es/uma/lcc/caesium/pedestrian/evacuation/optimization/ma/RunMAExitPlacement.java
@@ -0,0 +1,111 @@
+package es.uma.lcc.caesium.pedestrian.evacuation.optimization.ma;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+import com.github.cliftonlabs.json_simple.JsonException;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
+
+import es.uma.lcc.caesium.ea.base.EvolutionaryAlgorithm;
+import es.uma.lcc.caesium.ea.config.EAConfiguration;
+import es.uma.lcc.caesium.memetic.continuous.localsearch.MemeticContinuousVariationFactory;
+import es.uma.lcc.caesium.pedestrian.evacuation.optimization.ExitEvacuationProblem;
+import es.uma.lcc.caesium.pedestrian.evacuation.optimization.ea.CircularSetDiversity;
+import es.uma.lcc.caesium.pedestrian.evacuation.optimization.ea.PerimetralExitOptimizationFunction;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.configuration.SimulationConfiguration;
+import es.uma.lcc.caesium.pedestrian.evacuation.simulator.environment.Environment;
+
+/**
+ * Class for running the memetic evacuation optimization algorithm
+ * @author ccottap
+ * @version 1.0
+ */
+public class RunMAExitPlacement {
+	/**
+	 * environment filename prefix
+	 */
+	private static final String ENVIRONMENT_FILENAME = "base-";
+	/**
+	 * stats filename prefix
+	 */
+	private static final String STATS_FILENAME = "-stats-";
+	/**
+	 * solution simulation filename prefix
+	 */
+	private static final String SIMULATIONS_FILENAME = "-simulations-";
+	
+	
+	
+	/**
+	 * Main method
+	 * @param args command-line arguments
+	 * @throws JsonException if the configuration file is not correctly formatted
+	 * @throws IOException if files cannot be read/written
+	 */
+	public static void main(String[] args) throws JsonException, IOException {
+		// set US locale
+		Locale.setDefault(Locale.US);
+
+		EAConfiguration conf;
+		if (args.length < 4) {
+			System.out.println ("Required parameters: <configuration-name> <environment-name> <num-exits> <simulation-configuration>");
+			System.out.println ("\nNote that: ");
+			System.out.println ("\t- the EA configuration file will be sought as <configuration-name>.json,");
+			System.out.println ("\t- the environment configuration file will be sought as " + ENVIRONMENT_FILENAME + "<environment-name>.json,");
+			System.out.println ("\t- the statistics will be dumped to a file named <configuration-name>" + STATS_FILENAME + "<environment-name>-<num-exits>.json");
+			System.exit(1);
+		}
+				
+		// Configure the EA
+		FileReader reader = new FileReader(args[0] + ".json");
+		conf = new EAConfiguration((JsonObject) Jsoner.deserialize(reader));
+		int numruns = conf.getNumRuns();
+		long firstSeed = conf.getSeed();
+		conf.setVariationFactory(new MemeticContinuousVariationFactory());
+		System.out.println(conf);
+		EvolutionaryAlgorithm myEA = new EvolutionaryAlgorithm(conf);
+		myEA.setVerbosityLevel(0);
+		
+		// Configure the problem
+	    Environment environment = Environment.fromFile(ENVIRONMENT_FILENAME + args[1] + ".json");
+		SimulationConfiguration simulationConf = SimulationConfiguration.fromFile(args[3]);
+	    int numExits = Integer.parseInt(args[2]);
+	    ExitEvacuationProblem eep = new ExitEvacuationProblem (environment, numExits, simulationConf);
+	    PerimetralExitOptimizationFunction peof = new PerimetralExitOptimizationFunction(eep);
+		myEA.setObjectiveFunction(peof);
+		myEA.getStatistics().setDiversityMeasure(new CircularSetDiversity(1.0));
+		//System.out.println(eep);
+		System.out.println(simulationConf);
+		
+		for (int i=0; i<numruns; i++) {
+			long seed = firstSeed + i;
+			myEA.run(seed);
+			System.out.println ("Run " + i + ": " + 
+								String.format("%.2f", myEA.getStatistics().getTime(i)) + "s\t" +
+								myEA.getStatistics().getBest(i).getFitness());
+		}
+		PrintWriter file = new PrintWriter(args[0] + STATS_FILENAME + args[1] + "-" + args[2] + ".json");
+		file.print(myEA.getStatistics().toJSON().toJson());
+		file.close();
+		
+		// Analyze the best solutions more in depth
+		PrintWriter solsim = new PrintWriter(args[0] + SIMULATIONS_FILENAME + args[1] + "-" + args[2] + ".csv");
+		final int NUMSIMS = 1000;
+		solsim.print("run");
+		for (int j=0; j<NUMSIMS; j++)
+			solsim.print(",sim" + j);
+		solsim.println();
+		for (int i=0; i<numruns; i++) {
+			solsim.print(i);
+			var summaries = eep.simulate(peof.decode(myEA.getStatistics().getBest(i)), NUMSIMS); 
+			for (int j=0; j<NUMSIMS; j++)
+				solsim.print("," + eep.fitness(summaries.get(j)));
+			solsim.println();
+		}
+		solsim.close();
+		
+	}
+}
---
+++ src/main/java/es/uma/lcc/caesium/pedestrian/evacuation/optimization/ma/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Resolution of the evacuation problem with memetic algorithms
+ * @author ccottap
+ * @version 1.0
+ */
+package es.uma.lcc.caesium.pedestrian.evacuation.optimization.ma;
